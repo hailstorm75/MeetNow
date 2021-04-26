7c7fb67a2fd06ddc49bb25cf165d44ae10945375
Denis Akopyan <denis.akopyan@seznam.cz>
2021-04-26 20:55:49 +0200

    Add dynamic date creation

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,9 +5,16 @@
 @endsection
 
 @section('formStart')
-<form action="/events" method="POST" id="saveForm">
-    @csrf
-</form>
+    <form action="/events" method="POST" id="frm_saveForm">
+        @csrf
+    </form>
+    <form id="frm_date" onsubmit="onAddDateClicked()" method="POST">
+    </form>
+    <script>
+        const form = document.getElementById('frm_date');
+        function handleForm(event) {event.preventDefault()}
+        form.addEventListener('submit', handleForm)
+    </script>
 @endsection
 
 @section('eventName')
@@ -37,6 +44,72 @@
 @endsection
 
 @section("contentEvent")
+    <script>
+        class Date {
+            date;
+            time;
+            id;
+
+            get date() {
+                return this.date;
+            }
+
+            get time() {
+                return this.time;
+            }
+
+            constructor(id, date, time) {
+                this.id = id;
+                this.date = date;
+                this.time = time;
+            }
+        }
+
+        const dates = [];
+
+        function createUUID() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+
+        function onAddDateClicked() {
+            const id = createUUID();
+            const date = document.getElementById("date").value;
+            const time = document.getElementById("time").value;
+            const newDate = new Date(id, date, time);
+
+            const existingDates = dates.filter(function (e) {
+                return e.date === date && e.time === time;
+            })
+
+            console.log(existingDates);
+
+            if (existingDates.length > 0) {
+                alert("Please select a date that isn't created yet")
+                return;
+            }
+
+            const newRow = document.getElementById("tbl_date").insertRow(0);
+            newRow.id = id;
+
+            const a = newRow.insertCell(0);
+            const b = newRow.insertCell(1);
+            const c = newRow.insertCell(2);
+            a.innerHTML = date;
+            b.innerHTML = time;
+            c.innerHTML = "<button class='btn' onclick='onDeleteDateClicked(\"" + id + "\")'>Delete</button>"
+
+            dates.push(newDate);
+            console.log(dates);
+        }
+
+        function onDeleteDateClicked(id) {
+            const row = document.getElementById(id).rowIndex
+            document.getElementById("tbl_date").deleteRow(row - 1)
+        }
+    </script>
     <div class="col pl-0">
         <div class="row">
             <div class="col-2">
@@ -44,15 +117,15 @@
                     <div class="card-body">
                         <div class="form-group">
                             <label for="date">Date</label>
-                            <input type="date" id="date" class="w-100 mt-2">
+                            <input type="date" id="date" class="w-100 mt-2" required="true" form="frm_date">
                         </div>
                         <div class="form-group">
                             <label for="time">Time</label>
-                            <input type="time" id="time" class="w-100 mt-2">
+                            <input type="time" id="time" class="w-100 mt-2" required="true" form="frm_date">
                         </div>
                     </div>
                     <div class="card-footer">
-                        <button class="btn btn-primary w-100">
+                        <button class="btn btn-primary w-100" form="frm_date" type="submit">
                             <i class="fa fa-plus mr-lg-1" aria-hidden="true"></i>
                             <span class="d-none d-lg-inline">New date</span>
                         </button>
@@ -65,7 +138,18 @@
                         Dates
                     </div>
                     <div class="card-body">
-                        <em>TODO: Display added dates here</em>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody id="tbl_date">
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
